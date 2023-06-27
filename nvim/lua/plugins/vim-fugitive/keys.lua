ed0b5e9776f1a87f4a04ca4e47365b98103b5fa6
Alejandro Mc Ewen <alejandro.mcewen@macheight.com>
2023-06-26 20:59:37 -0500

    Finally got it right

--- nvim/lua/plugins/vim-fugitive/keys.lua
+++ nvim/lua/plugins/vim-fugitive/keys.lua
@@ -1,6 +1,6 @@
 local Keys = {
 	{ "<leader>ga", "<cmd>Git add .<cr>", noremap = true, desc = "add to git" },
-	{ "<leader>gs", "<cmd>Git status<cr>", noremap = true, desc = "git status" },
+	{ "<leader>gs", "<cmd>Git<cr>", noremap = true, desc = "git status" },
 	{ "<leader>gc", "<cmd>Git commit<cr>", noremap = true, desc = "commit to git" },
 	{ "<leader>gp", "<cmd>Git push<cr>", noremap = true, desc = "push to remote repository" },
 	{ "<leader>gd", "<cmd>Git diff<cr>", noremap = true, desc = "git diff" },
