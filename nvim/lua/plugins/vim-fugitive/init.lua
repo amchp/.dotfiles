bcd5942e5212eddf4514404763936f8bab01208a
Alejandro Mc Ewen <alejandro.mcewen@macheight.com>
2023-06-26 20:52:47 -0500

    New commit

--- nvim/lua/plugins/vim-fugitive/init.lua
+++ nvim/lua/plugins/vim-fugitive/init.lua
@@ -1,7 +1,6 @@
 local Plugin = {
 	"tpope/vim-fugitive",
 	dependencies = { { "EdenEast/nightfox.nvim" } },
-	cmd = { "Git status", "Git add", "Git commit", "Git push", "Git diff" },
 	keys = function()
 		return require("plugins.vim-fugitive.keys")
 	end,
