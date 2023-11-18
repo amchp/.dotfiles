e502ed337251e2bab19aac328de8818b91b7e482
Alejandro McEwen <juanmcewen@Juans-Mac-mini.local>
2023-11-17 22:05:33 -0500

    Removing some unused plugins

--- nvim/lua/plugins/vim-twiggy/keys.lua
+++
@@ -1,5 +0,0 @@
-local Keys = {
-	{ "<leader>gb", "<cmd>Twiggy<cr>", desc = "open files picker" },
-}
-
-return Keys
--- nvim/lua/plugins/vim-twiggy/opts.lua
+++
@@ -1,6 +0,0 @@
-local Opts = {
-	twiggy_group_locals_by_slash = 0,
-	twiggy_local_branch_sort = "mru",
-	twiggy_remote_branch_sort = "date",
-}
-return Opts
