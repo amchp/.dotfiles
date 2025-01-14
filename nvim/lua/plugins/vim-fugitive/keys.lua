17313673bc1ef01059df7fc5a5efe923078d5809
Alejandro McEwen <juanmcewen@Juans-Mac-mini.local>
2025-01-14 14:46:02 -0500

    Adding fugitive branch

--- nvim/lua/plugins/vim-fugitive/keys.lua
+++ nvim/lua/plugins/vim-fugitive/keys.lua
@@ -3,7 +3,7 @@ local Keys = {
 	{ "<leader>gs", "<cmd>Git<cr>", noremap = true, desc = "git status" },
 	{ "<leader>gc", "<cmd>Git commit<cr>", noremap = true, desc = "commit to git" },
 	{ "<leader>gn", "<cmd>Git commit --no-verify<cr>", noremap = true, desc = "commit to git no verify" },
-	{ "<leader>gp", "<cmd>Git push -u<cr>", noremap = true, desc = "push to remote repository" },
+    { "<leader>gp", "<cmd>Git push -u origin $(Git branch --show-current)<cr>", noremap = true, desc = "push to remote repository with branch" },
 	{ "<leader>gd", "<cmd>Gdiff :0<cr>", noremap = true, desc = "git diff" },
 }
 return Keys
