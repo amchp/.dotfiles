e89adfed38f1742ddc196510d665239990c5199b
Alejandro Mc Ewen Work <alejandromcewenwork@Angelas-MacBook-Air-2.local>
2025-01-16 10:48:48 -0500

    Adding raycast command

---
+++ raycast/open-raycast.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open Raycast
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description Another way to open Raycast
+# @raycast.author Alejandro Mc Ewen
+# @raycast.authorURL https://amc-page.vercel.app/
+
+open /Applications/Raycast.app
