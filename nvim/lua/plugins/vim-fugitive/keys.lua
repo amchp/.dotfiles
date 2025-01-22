85cdf9614641513e24e71d4ab05ba4f26360f2b8
Alejandro McEwen <juanmcewen@Juans-Mac-mini.local>
2025-01-22 14:41:42 -0500

    Fixing git commit

--- nvim/lua/plugins/vim-fugitive/keys.lua
+++ nvim/lua/plugins/vim-fugitive/keys.lua
@@ -3,7 +3,16 @@ local Keys = {
 	{ "<leader>gs", "<cmd>Git<cr>", noremap = true, desc = "git status" },
 	{ "<leader>gc", "<cmd>Git commit<cr>", noremap = true, desc = "commit to git" },
 	{ "<leader>gn", "<cmd>Git commit --no-verify<cr>", noremap = true, desc = "commit to git no verify" },
-    { "<leader>gp", "<cmd>Git push -u origin $(Git branch --show-current)<cr>", noremap = true, desc = "push to remote repository with branch" },
+	{
+		"<leader>gp",
+		function()
+			local branch = vim.fn.system("git branch --show-current"):gsub("%s+", "")
+			vim.cmd("Git push -u origin " .. branch)
+		end,
+		noremap = true,
+		desc = "push to remote repository with branch",
+	},
 	{ "<leader>gd", "<cmd>Gdiff :0<cr>", noremap = true, desc = "git diff" },
 }
 return Keys
+
